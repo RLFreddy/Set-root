972328cb3c0d7cc62c1afac10c6ca41538464a2d
RL Freddy <efreddyrl@gmail.com>
2022-07-22 00:27:04 -0500

    first commit

---
+++ root.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+clear
+# By crazy_vpn
+# Pequeno script para permissao de autenticacao root!!
+
+[[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "without-password" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PermitRootLogin" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication" /etc/ssh/sshd_config) = '0' ]] && {
+	echo 'PasswordAuthentication yes' > /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+[[ $(grep -c "#PasswordAuthentication no" /etc/ssh/sshd_config) != '0' ]] && {
+	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+} > /dev/null
+service ssh restart > /dev/null
+clear; echo -e "\033[43m BY CRAZYVPN || LACASITAMX\e[0m"; echo ""; echo -e "\033[1;33mINGRESE SU NUEVA CONTRASEÑA PARA TENER ACCESO A ROOT\033[0m"; sleep 2s; read pass
+(echo $pass; echo $pass)|passwd 2>/dev/null
+clear; echo -e "\e[1;31mSU NUEVA CONTRASEÑA ES:\e[1;32m $pass"
